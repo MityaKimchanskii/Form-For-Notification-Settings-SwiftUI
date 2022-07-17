01db0d7257332c7d54284e2cf60728f62367931c
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-17 15:54:08 -0700

    Initial Commit

---
+++ Form-For-Notification-Settings-SwiftUI/Form_For_Notification_Settings_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Form_For_Notification_Settings_SwiftUIApp.swift
+//  Form-For-Notification-Settings-SwiftUI
+//
+//  Created by Mitya Kim on 7/17/22.
+//
+
+import SwiftUI
+
+@main
+struct Form_For_Notification_Settings_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
