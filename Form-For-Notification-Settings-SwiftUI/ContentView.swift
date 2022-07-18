6fa3b242977a84fb5a24d4dd0e68b7bbc851a9d9
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-18 12:02:36 -0700

    final commit

--- Form-For-Notification-Settings-SwiftUI/ContentView.swift
+++ Form-For-Notification-Settings-SwiftUI/ContentView.swift
@@ -8,9 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let images = ["🎆","🗾","🏤","🏦","🏞","🌅","🌇","🏙","🌌","🏢"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+            
+       
+        NavigationView {
+            
+            Form {
+                
+                Section {
+                    HStack {
+                        
+                        Text("Show Previews")
+                        Spacer()
+                        NavigationLink {
+                            
+                        } label: {
+                            Text("Always").foregroundColor(.gray)
+                        }.fixedSize()
+                    }
+                } header: {
+                    Text("")
+                }
+
+                Section {
+                    NavigationLink {
+                        
+                    } label: {
+                        Text("Siri Suggestions")
+                    }
+                } header: {
+                    Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+                }
+                
+                Section {
+                    ForEach(images, id: \.self) { image in
+                        
+                        HStack {
+                            
+                            Text(image)
+                                .font(.largeTitle)
+                            Text("Notification")
+                        }
+                    }
+                } header: {
+                    Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+                    Text("Notification Style")
+                }
+            }
+            .navigationTitle("Notification")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
